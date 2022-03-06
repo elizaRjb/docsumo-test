ab3f2c6e3c87eb95f14fd471238989a62f42d333
Eliza Rajbhandari <eliza.rajbhandari@gmail.com>
2022-03-06 16:47:28 +0545

    Add TextInput component

---
+++ src/components/TextInput/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function TextInput(props) {
+  const {
+    className,
+    name,
+    value,
+    onChange,
+    label,
+    errorMsg,
+    disabled,
+    type = "text",
+  } = props;
+  const [isActive, setIsActive] = useState(false);
+  
+  const handleFocus = () => {
+    setIsActive(true);
+  };
+
+  const handleBlur = () => {
+    if (value) {
+      return;
+    }
+    setIsActive(false);
+  };
+
+  const labelClassName = isActive
+    ? "text-input__label text-input__label--active"
+    : "text-input__label";
+
+  return (
+    <div className={`text-input ${className ? className : ""}`}>
+      <label className={labelClassName}>{label}</label>
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        disabled={disabled}
+      />
+      {errorMsg && <span className="text-input__error-msg">{errorMsg}</span>}
+    </div>
+  );
+}
+
+export default TextInput;
