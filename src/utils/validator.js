2bc73642294fefc6c72857452cd48d7b81889dfc
Eliza Rajbhandari <eliza.rajbhandari@gmail.com>
2022-03-07 12:17:23 +0545

    Minor fix

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,4 +1,4 @@
-const EMAIL_REGEX = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+const EMAIL_REGEX = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
 
 class Validator {
   checkIsEmpty(value) {
@@ -11,9 +11,9 @@ class Validator {
 
   validateEmail(email) {
     if (!email.trim()) {
-      return { isValid: false, msg: "Please enter your email address" };
+      return { isValid: false, msg: 'Please enter your email address' };
     } else if (!email.trim().match(EMAIL_REGEX)) {
-      return { isValid: false, msg: "Please enter a valid email address" };
+      return { isValid: false, msg: 'Please enter a valid email address' };
     } else {
       return { isValid: true };
     }
